e7703c8b85f93fd3b2b870648b0128172f1ef6ae
Alexey Gaydukov <dvoalex@gmail.com>
2016-04-18 08:08:49 +1200

    Delete bad addresses too

--- _all_files/projects/powershell/dhcpkickmob.ps1
+++ _all_files/projects/powershell/dhcpkickmob.ps1
@@ -3,4 +3,5 @@
 # Date: 14/04/2016
 # Description: gets list of "iphone" and "android" devices from DHCP scope 192.168.2.0 and kick these devices off from DHCP scope lease.
 
-netsh dhcp server scope 192.168.2.0 show clients 1 | findstr "iphone android" | %{$_.split(' ')[0]} | %{netsh dhcp server scope 192.168.2.0 delete lease $_}
+netsh dhcp server scope 192.168.2.0 show clients 1 | findstr "iphone iPhone android" | %{$_.split(' ')[0]} | %{netsh dhcp server scope 192.168.2.0 delete lease $_}
+netsh dhcp server scope 192.168.2.0 delete allbadaddresses
