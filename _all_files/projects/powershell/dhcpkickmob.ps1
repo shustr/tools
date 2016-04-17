44ea7d1a3eb316208f6ef3d0d3099cb07db2dae5
Alexey Gaydukov <dvoalex@gmail.com>
2016-04-18 08:46:00 +1200

    Syntax fix

--- _all_files/projects/powershell/dhcpkickmob.ps1
+++ _all_files/projects/powershell/dhcpkickmob.ps1
@@ -4,4 +4,4 @@
 # Description: gets list of "iphone" and "android" devices from DHCP scope 192.168.2.0 and kick these devices off from DHCP scope lease.
 
 netsh dhcp server scope 192.168.2.0 show clients 1 | findstr "iphone iPhone android" | %{$_.split(' ')[0]} | %{netsh dhcp server scope 192.168.2.0 delete lease $_}
-netsh dhcp server scope 192.168.2.0 delete allbadaddresses
+netsh dhcp server scope 192.168.2.0 delete lease allbadaddresses
