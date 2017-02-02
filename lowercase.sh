04713d39e5a58e2ec7359da7779d49eb69c4fa1c
shustr <dvoalex@gmail.com>
2017-02-03 12:19:45 +1300

    edited description

--- lowercase.sh
+++ lowercase.sh
@@ -1,8 +1,11 @@
-# Purpose of the script:
-# 1. Renames all files in destination directory to lower case names
-# 2. If file name exists (for example, Filename.txt and filename.txt presented), script performs content comparison. If the files are same, overwhites. If different, renames Filename.txt into filename_01.txt
+#  Purpose of the script:
+#  1. Renames all files in destination directory to lower case names
+#  2. If file name exists (for example, Filename.txt and filename.txt presented), script performs content comparison. 
+#  3. If the files are same, overwhites. If different, renames Filename.txt into filename_01.txt or next number if this one exists. 
+#     Example: if filename_01 and filename_02 exist, it would rename source file into filename_03
 #
-# Current functionality: 1. only
+#
+# Current functionality: 1 (only renames files and asking to overwite if file already exists)
 
 
 #!/bin/bash
