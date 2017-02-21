db29d7a8f36ceb8b76ee25aa7e8e6fb485b506e5
shustr <dvoalex@gmail.com>
2017-02-21 14:44:48 +1300

    changes

--- _all_files/projects/powershell/dhcpkickmob.ps1
+++
@@ -1,7 +0,0 @@
-# Ver. 0.1
-# Author: alex.gaydukov@spark.co.nz
-# Date: 14/04/2016
-# Description: gets list of "iphone" and "android" devices from DHCP scope 192.168.2.0 and kick these devices off from DHCP scope lease.
-
-netsh dhcp server scope 192.168.2.0 show clients 1 | findstr "iphone iPhone android" | %{$_.split(' ')[0]} | %{netsh dhcp server scope 192.168.2.0 delete lease $_}
-netsh dhcp server scope 192.168.2.0 delete lease allbadaddresses
