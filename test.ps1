bca5caa49e8919ee0a59a95c816f96c4f4af5d2a
shustr <dvoalex@gmail.com>
2017-06-02 10:37:21 +1200

    test commit

--- test.ps1
+++ test.ps1
@@ -1,5 +1,5 @@
 $SourceFilePath = "C\:test.txt" # file contains output of "net use" command
-
+# test comment
 #greps (at least supposed to) column for Disk name
 $SourceFileContentDisk = Get-Content $SourceFilePath | findstr "\\" | %{ $_.Split(':')[0]; }
 $SourceFileContentDisk.Trim() | %{ $_.Split('\s')[0]; }
