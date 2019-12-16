4dfd6ad36195f77d61fed4e35a96644744b283ab
shustr <dvoalex@gmail.com>
2019-12-17 09:23:10 +1300

    test 2

--- bulkIPcheck.sh
+++ bulkIPcheck.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 # bulkping
 date
-# takings IP addresses from the file.txt
+# taking IP addresses from the file.txt
 grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' file.txt > iplist.txt
+
+# does ping for each IP and gives result 
 cat iplist.txt |  while read output
 do
  ping -c 1 "$output" > /dev/null
      if [ $? -eq 0 ]; then
-     echo "$output is up"
+     echo "$output is UP"
      else
      echo "$output is down"
      fi
